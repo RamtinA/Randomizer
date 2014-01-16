8646acb8ce7ced850db23b2f81ce608368a5c317
RamtinA <ramtin.number1@gmail.com>
2014-01-16 15:13:37 +0330

    Work on GUI

--- randomizer.py
+++ randomizer.py
@@ -16,6 +16,7 @@ class AppWindow(Gtk.Window):
 		AddNameVBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=6)
 		RandomNameVBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=18)
 
+		DeleteNameVBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=10)
 		#H Boxes
 		AddNameHBox = Gtk.Box (spacing = 10)
 
@@ -30,23 +31,41 @@ class AppWindow(Gtk.Window):
 		StackSwitcher.set_stack(Stack)
 
 		#Lables
-		NameLabel = Gtk.Label("Name :")
-		RandomLabel = Gtk.Label("Ready?")
+		self.NameLabel = Gtk.Label("Name :")
+		self.RandomLabel = Gtk.Label("Ready?")
 
 		#Entry
-		NameEntry = Gtk.Entry()
+		self.NameEntry = Gtk.Entry()
 
 		#Buttons
-		AddNameButton = Gtk.Button("Add Name")
-		RandomButton = Gtk.Button("Select By Random")
+		self.AddNameButton = Gtk.Button("Add Name")
+		self.RandomButton = Gtk.Button("Select By Random")
+		self.DeleteButton = Gtk.Button("Delete Name")
+
+		#Combobox
+		self.Names =["test","testsdtsdf","dsfsdf"]
+		NameStore = Gtk.ListStore(str)
+		self.Names.append("asdasd")
+
+		self.NameCombo = Gtk.ComboBox.new_with_model(NameStore)
+		self.NameCombo.set_entry_text_column(1)
+
+		for Name in self.Names:
+			NameStore.append([Name])
+
+		RendererText = Gtk.CellRendererText()
+		self.NameCombo.pack_start(RendererText,True)
+		self.NameCombo.add_attribute(RendererText,"text",0)
+
 
 		#Add H Boxes
-		AddNameHBox.pack_start(NameLabel,True,True,0)
-		AddNameHBox.pack_start(NameEntry,True,True,0)
+		AddNameHBox.pack_start(self.NameLabel,True,True,0)
+		AddNameHBox.pack_start(self.NameEntry,True,True,0)
 
 		#Add Stacks
 		Stack.add_titled(AddNameVBox,"Add","Add Name")
 		Stack.add_titled(RandomNameVBox,"Random","Random Name")
+		Stack.add_titled(DeleteNameVBox,"Delete","Delete Name")
 
 
 		#Add V Boxes
@@ -54,11 +73,13 @@ class AppWindow(Gtk.Window):
 		VBox.pack_start(Stack,True,True,0)
 		
 		AddNameVBox.pack_start(AddNameHBox,True,True,0)
-		AddNameVBox.pack_start(AddNameButton,True,True,0)
+		AddNameVBox.pack_start(self.AddNameButton,True,True,0)
 
-		RandomNameVBox.pack_start(RandomLabel,True,True,0)
-		RandomNameVBox.pack_start(RandomButton,True,True,0)
+		RandomNameVBox.pack_start(self.RandomLabel,True,True,0)
+		RandomNameVBox.pack_start(self.RandomButton,True,True,0)
 
+		DeleteNameVBox.pack_start(self.NameCombo,True,True,0)
+		DeleteNameVBox.pack_start(self.DeleteButton,True,True,0)
 		
 
 		
